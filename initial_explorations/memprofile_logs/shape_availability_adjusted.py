253599445e25b308d1d99f895691ae2a640e0c8c
calvintr <calvintriems@aol.com>
2022-06-02 21:39:04 +0200

    Add proper commenting.

--- initial_explorations/memprofile_logs/shape_availability_adjusted.py
+++ initial_explorations/memprofile_logs/shape_availability_adjusted.py
@@ -27,8 +27,11 @@ def shape_availability(geometry, excluder):
 
     bounds = rio.features.bounds(geometry)
     transform, shape = padded_transform_and_shape(bounds, res=excluder.res)
-#    masked = geometry_mask(geometry, shape, transform).astype(int)
+    
+    # First transformation from bool to int32 that is skipped
+    # masked = geometry_mask(geometry, shape, transform).astype(int)
     masked = geometry_mask(geometry, shape, transform)
+    
     exclusions.append(masked)
 
     # For the following: 0 is eligible, 1 in excluded
@@ -51,20 +54,26 @@ def shape_availability(geometry, excluder):
             masked_ = masked
 
         if d["invert"]:
-#            masked_ = ~(masked_).astype(bool)
-            masked_ = ~(masked_)
+            masked_ = ~(masked_).astype(bool)
         if d["buffer"]:
             iterations = int(d["buffer"] / excluder.res) + 1
-#            masked_ = dilation(masked_, iterations=iterations).astype(int)
+            
+            # Second bool to int transformation that is skipped 
+            # masked_ = dilation(masked_, iterations=iterations).astype(int)
             masked_ = dilation(masked_, iterations=iterations)
 
-#        exclusions.append(masked_.astype(int))
+        # Third bool to int transformation skipped. Transforming and appending to list might cause
+        # doubling of matrix in memory. 
+        # exclusions.append(masked_.astype(int))
         exclusions.append(masked_)
 
     for d in excluder.geometries:
         masked = ~geometry_mask(d["geometry"], shape, transform, invert=d["invert"])
-#        exclusions.append(masked.astype(int))
+        
+        # Fourth bool to int transformation skipped.
+        # exclusions.append(masked.astype(int))
         exclusions.append(masked)
     
-#    return (sum(exclusions) == 0).astype(float), transform
+    # bool to float transformation skipped
+    # return (sum(exclusions) == 0).astype(float), transform
     return (sum(exclusions) == 0), transform
