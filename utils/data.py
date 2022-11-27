10d77a3d82a489316181b6c9e56b6f9260210976
calvintr <calvintriems@aol.com>
2022-11-27 21:54:28 +0100

    major updates to utils data.py and geo.py

--- utils/data.py
+++ utils/data.py
@@ -17,8 +17,72 @@ from cartopy.crs import PlateCarree as plate
 import cartopy.io.shapereader as shpreader
 import shapely
 
+from osgeo import gdal
+from osgeo import gdalconst
+from osgeo import ogr
+
 from utils import geo 
 
+def burn_vector_in_raster(raster, vector, vector_attribute ,out_dtype, nodata_val, out_path):
+
+    ndsm = raster
+    shp = vector 
+    dta = gdal.Open(ndsm, gdalconst.GA_ReadOnly)
+    geo_transform = dta.GetGeoTransform()
+    #source_layer = dta.GetLayer()
+    x_min = geo_transform[0]
+    y_max = geo_transform[3]
+    x_max = x_min + geo_transform[1] * dta.RasterXSize
+    y_min = y_max + geo_transform[5] * dta.RasterYSize
+    x_res = dta.RasterXSize
+    y_res = dta.RasterYSize
+    mb_v = ogr.Open(shp)
+    mb_l = mb_v.GetLayer()
+    pixel_width = geo_transform[1]
+    output = out_path 
+    target_ds = gdal.GetDriverByName('GTiff').Create(output, x_res, y_res, 1, gdal.GDT_Float32)
+    target_ds.SetGeoTransform((x_min, pixel_width, 0, y_min, 0, pixel_width))
+    band = target_ds.GetRasterBand(1)
+    NoData_value = nodata_val
+    band.SetNoDataValue(NoData_value)
+    band.FlushCache()
+    gdal.RasterizeLayer(target_ds, [1], mb_l, options=[f"ATTRIBUTE={vector_attribute}"])
+
+    target_ds = None
+    
+
+def write_geotiff(filename, mask, trans, proj = 'epsg:3035', gdal_write_dtype = gdal.GDT_Float32):
+    """
+    Write np.array to GeoTiff rasterfile. Utilizes mask and transfrom objectes returned from atlite.shape_availibility()
+    
+    Parameters
+    ----------
+    
+    filename : 'str'
+    mask : numpy.ndarray
+        Array containing raster matrix returned from shape_availability()[0]
+    trans : Affine()
+        Geometry information returned from shape_availability()[1]
+    proj : str()
+        Projection valid by gdal.driver.SetProjection()
+        Default: 'epsg:3035'
+    gdal_write_dtype : int (GDAL dtype index)
+        Information on which dtype to use when writing geotiff.
+        Available types: https://naturalatlas.github.io/node-gdal/classes/Constants%20(GDT).html
+        Most common: gdal.GDT_Float32, gdal.GDT_Byte, gdal.GDT_UInt16
+    """
+    
+    arr_type = gdal_write_dtype
+
+    driver = gdal.GetDriverByName("GTiff")
+    
+    out_ds = driver.Create(filename, mask.shape[1], mask.shape[0], 1, arr_type)
+    out_ds.SetProjection(proj)
+    out_ds.SetGeoTransform([trans[2], trans[0], trans[1], trans[5], trans[3], trans[4]])
+    
+    band = out_ds.GetRasterBand(1)
+    band.WriteArray(mask)
+    band.FlushCache()
 
 def cutout_download(country, start, end, path, feature = None, freq = 'M', shape_filter = None):
     """
@@ -70,7 +134,32 @@ def cutout_download(country, start, end, path, feature = None, freq = 'M', shape
         country_shp = geo.tif_bb_filter(raster,
                                     	gpd.GeoDataFrame(country_shp, columns = ['geometry']),
                                     	output_crs = 'shapes')
-        
+    
+    #Exclude Canary Islands
+    if country == 'Spain':
+        bounds = country.geometry.bounds.values[0]
+        bb = box(bounds[0], 34, bounds[2], bounds[3])
+        bb = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[bb])
+        country_shp = gpd.overlay(country, bb, how = 'intersection')
+
+    # Exclude Madeira
+    if country == 'Portugal':
+        bounds = country.geometry.bounds.values[0]
+        bb = box(-15, bounds[1], bounds[2], bounds[3])
+        bb = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[bb])
+        country_shp = gpd.overlay(country, bb, how = 'intersection')
+
+    # Exclude Jan Mayen
+    if country == 'Norway':
+        bounds = country.geometry.bounds.values[0]
+        bb = box(0, bounds[1], bounds[2], bounds[3])
+        bb = gpd.GeoDataFrame(index=[0], crs='epsg:4326', geometry=[bb])
+        country_shp = gpd.overlay(country, bb, how = 'intersection')
+    
+    #Retrieve bounds of country shape + buffer for download
+    bounds = country_shp.bounds.values[0]
+    bb = shapely.geometry.box(bounds[0]-0.125, bounds[1]-0.125, bounds[2]+0.125, bounds[3]+0.125)
+    bb = gpd.GeoDataFrame(gpd.GeoSeries(bb), columns=['geometry'])
               
     #Loop Over DT Range
     for i in dt:
@@ -83,7 +172,7 @@ def cutout_download(country, start, end, path, feature = None, freq = 'M', shape
 
         cutout = atlite.Cutout(path=savepath,
                                module='era5',
-                               bounds=country_shp.unary_union.bounds,
+                               bounds=bb.unary_union.bounds,
                                time=i)
         
         if feature != None:
--- utils/geo.py
+++ utils/geo.py
@@ -10,12 +10,152 @@ import os.path
 import pandas as pd
 from pandas.tseries.offsets import MonthEnd
 import numpy as np
+import math
+
 import geopandas as gpd
 
 import cartopy.crs as ccrs
 from cartopy.crs import PlateCarree as plate
 import cartopy.io.shapereader as shpreader
 import shapely
+
+from atlite.gis import shape_availability
+
+from skimage import draw, morphology
+from scipy.ndimage import binary_dilation as dilation
+from scipy.ndimage import convolve 
+import math
+
+import time
+
+
+def reduce(input_array, initial_total, reduction_target, kernel, sensi_start, threshold = 0.005):
+    
+    main = input_array
+    target = input_array.sum()/initial_total - reduction_target
+    current = 0
+    sensitivity = sensi_start
+    k = kernel[0]
+    
+
+    print(input_array.sum())
+    
+    while abs(target - current) > threshold:
+           
+        b = convolve(main.astype(int), k, mode='constant')                   
+        b = np.where(b < sensitivity, 0, 1).astype(bool)
+                
+        a = main & b
+                                           
+        if (abs(a.sum()/initial_total) + threshold < target) or \
+           (abs(a.sum()/initial_total - current) == 0):
+            
+            sensitivity = sensitivity-1
+            print(f'Sensitivity adjustment: {sensitivity}')
+            
+            if sensitivity == 0:
+                
+                if k.sum() == kernel[1].sum():
+                    break
+                    
+                k = kernel[1]
+                sensitivity = k.sum() 
+                print('Kernel 2 activated')
+                
+            continue
+            
+        main = a
+        current = main.sum()/initial_total
+        print(f'New Best: {current}')
+        
+    return main
+
+
+def place_n_turbines(res, 
+                     landuse_av, 
+                     wind_dir, 
+                     turbine_type, turbine_dict, 
+                     r_radius_factor, c_radius_factor):    
+    
+    ## Set-up initial matrices   
+    #landuse
+    landuse = landuse_av.copy()
+    
+    #wind direction
+    if np.isscalar(wind_dir):
+        wind = np.full(landuse_av.shape, wind_dir).astype(int)
+    else:
+        wind = wind_dir
+    
+    #turbine elipses
+    #turbine_elipse = ~np.zeros(shape = landuse.shape, dtype = int)
+    
+    #turbine placements
+    turbine = np.zeros(shape = landuse.shape, dtype = 'uint8') 
+    
+    ##Pre-calculate radii for indvidual turbine types
+    turbine_precalc = {} 
+    for i in turbine_dict.keys():
+        turbine_precalc[i] = {'r' : round(turbine_dict[i]/res*r_radius_factor, 0), 
+                              'c' : round(turbine_dict[i]/res*c_radius_factor, 0)}
+    
+    
+    for i in sorted(turbine_dict.keys())[::-1]:
+    
+        tmp = np.zeros(shape = landuse.shape, dtype = bool) 
+        a, b = np.where((landuse==1) & (turbine_type==i))
+        tmp[a,b] = True
+
+        n = 0
+        start = time.time()
+        row_last = 0
+        
+        ## Loop operation
+        while True:
+            
+            n=n+1
+
+            # Find first available index from NW 
+            idx = np.argmax(tmp[row_last:, :], axis=None)
+            idx = np.unravel_index(idx, landuse.shape)
+            idx = (idx[0] + row_last, idx[1])
+            
+            # Save the last row where a turbine was placed to prevent full array search
+            row_last = idx[0]
+            
+            #If no more turbines to place, quit loop
+            if tmp[idx] == 0:
+                break
+            
+            #idx = np.unravel_index(np.argmax(tmp, axis=None), landuse.shape)
+            #if idx == (0,0):
+            #    break
+
+            # Retrieve favourable turbine characteristics and wind direction at index
+            t = turbine_type[idx[0], idx[1]]
+            r = wind[idx[0], idx[1]]
+
+            # Save turbine placement center & type
+            turbine[idx[0] , idx[1]] = t
+
+            # Draw elipse based on predominant wind direcition and turbine characteristics   
+            rr, cc = draw.ellipse(idx[0], idx[1], turbine_precalc[t]['c'], turbine_precalc[t]['r'] , landuse.shape , 
+                                  rotation = r)
+
+            landuse[rr,cc] = False
+            tmp[rr,cc] = False
+            #turbine_elipse[rr,cc] = t
+            
+            if n == 10000:
+                now = time.time()
+                print('10000: {0} seconds'.format(round(now-start,2)))
+                n=0
+                start = time.time()
+    
+    print(f'Successfully built {np.where(turbine>0,1,0).sum()} turbines')
+    
+    return [landuse, turbine]#, turbine_elipse] 
+
 
 def merge_cutouts(src):
     """
@@ -70,3 +210,60 @@ def tif_bb_filter(raster, shapes, output_crs = 'raster'):
         shapes_filtered = shapes_filtered.to_crs(shapes.crs)
 
     return shapes_filtered
+
+
+def dn_to_degrees(dn):
+    
+    """
+    Convert GISCO slope dataset file falues (dn) to slope values according to
+    https://ec.europa.eu/eurostat/documents/7116161/7172326/SPEC011-b140109-SLOP.pdf
+    
+    Parameters:
+    -----------
+    
+    dn : int
+        dn value between 0 and 250
+        
+    Returns:
+    --------
+    
+    corresponding slope degree : float16
+    
+    """
+    
+    return np.float16(np.arccos(dn/250.0)*180/math.pi)
+    
+
+def mte(exc, country, share_kind = "normal"):
+    """
+    Function that wraps the calculation of mask and transform affine output from atlite.shape_availibility()
+    as well as the calculation of the eligible_share
+    
+    Parameters
+    __________
+    
+    exc = atlite.ExculsionContainer
+    country = gpd.DataFrame
+    share_kind = str
+    
+    Returns
+    _______
+    
+    list where,
+    [0] = masked (array)
+    [1] = transform (Affine())
+    [2] = eligible_area (num)
+    [3] = eligible_share (num)  
+    """
+    
+    masked, transform = shape_availability(country, exc)
+    eligible_area = masked.astype(float).sum() * exc.res**2
+
+    if share_kind == "normal":
+        eligible_share =  eligible_area / country.geometry.item().area
+    elif share_kind == "inverse":
+        eligible_share =  1-(eligible_area / country.geometry.item().area)
+
+    mte_results = [masked, transform, eligible_area, eligible_share]
+
+    return mte_results
